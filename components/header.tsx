b3789b84af1c6ae34a23d11fb0f8962a1ded90b3
WhatTheShuck <Contact@brandonwiedman.com>
2025-05-13 14:43:26 +1000

    Fix Typo from commit merge

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { companyDetails } from "@/lib/data";
 import { ModeToggle } from "./theme-toggle";
-import { ProfileButton } from "./profileButton";
+import { ProfileButton } from "./ProfileButton";
 
 const Header = async () => {
   return (
