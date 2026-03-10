d3a1c11a822ed6a15023a21079f8ccbee8210eb4
WhatTheShuck <Contact@brandonwiedman.com>
2026-03-10 11:04:00 +1000

    Fix caching issue in sign-out causing the profile button to have stale data

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -9,7 +9,13 @@ export function SignOut() {
   return (
     <button
       className="flex w-full items-center text-sm"
-      onClick={() => startTransition(() => { void signOut(); })}
+      onClick={() =>
+        startTransition(() => {
+          void signOut().then(() => {
+            window.location.href = "/auth";
+          });
+        })
+      }
       disabled={isPending}
     >
       <LogOut className="mr-2 h-4 w-4" />
